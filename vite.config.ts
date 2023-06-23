afad18bccd0e3b8816d991a92df497312f88fcb6
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2023-06-23 06:38:32 +0300

    vite.config has been updated 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import reactIs from "react-is";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "react-is": reactIs,
-    },
-  },
 });
